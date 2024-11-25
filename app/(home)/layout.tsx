7182cf1b55fad839a269c9e071b64d8f8fb6dd68
sohan2410 <bandarysohan24@gmail.com>
2024-11-25 15:56:13 +0530

    Added Theme Toggle mode

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,11 +2,15 @@
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
 import { UserMenu } from "@/components/custom/user-menu";
+import { ModeToggle } from "@/components/theme/toggle-mode";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <main className="container mx-auto flex min-h-screen flex-col gap-4 p-4 sm:p-16">
-
+        <div className="absolute top-4 right-4 sm:top-16 sm:right-16">
+          <ModeToggle />
+        </div>
         <div className="flex flex-col gap-4 sm:gap-8 w-full max-w-[100rem] mx-auto relative min-h-full h-full rounded-lg border border-border/50 bg-background/50 p-4 backdrop-blur-[2px] sm:p-8">
           <div className="flex justify-between items-center">
             <div className="grid gap-1">
